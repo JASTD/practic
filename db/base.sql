b4ab76a6159b5a275719b4fc5e49eb78f3b379be
JASTD <egorlinnik05@gmail.com>
2024-02-20 10:10:17 +0300

    Update base.sql

--- db/base.sql
+++ db/base.sql
@@ -1,7 +1,20 @@
+drop database zd;
 create database if not exists zd;
 
 use zd;
 
+-- создание таблицы администратора, который будет работать с БД 
+create table administrator(
+admin_id int not null primary key auto_increment,
+name_a varchar(100) not null,
+surname_e varchar(100) not null,
+patronymic_e varchar(100),
+login varchar(100) not null,
+password_a varchar(100) not null
+
+);
+
+-- создание таблицы сотрудики(обслуживают маршруты и поезда) 
 create table employeers(
 id_e int not null primary key auto_increment,
 name_e varchar(100) not null,
@@ -14,27 +27,36 @@ phone_e varchar(12) not null
 
 
 
-
+-- создание таблицы поездов
 create table trains(
 train_id int not null primary key auto_increment,
 name_t varchar(100) not null,
 number_t int not null,
 hp_t int not null,
 odometr_t decimal(6,1), 
-responsible_t int
-
+responsible_t int,
+foreign key (responsible_t) references employeers(id_e) 
 );
 
 
+-- создание таблицы маршруты
 create table route(
 id_r int not null primary key auto_increment,
 exit_point_r varchar(100) not null,
 finish_point_r varchar(100) not null,
 goal_r varchar(100) not null,
 price decimal(5,1),
-kilometrage_r decimal(6,1),
-responsible_r int, 
+kilometrage_r decimal(6,1), 
 train_r int,
+foreign key (train_r) references trains(train_id)
+);
 
+-- создание таблицы справки 
+create table spravki(
+id_s int not null primary key auto_increment,
+otprav_s datetime,
+pib_s datetime,
+route_fk int,
+foreign key (route_fk) references route(id_r)
 );
 
