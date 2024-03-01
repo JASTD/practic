c2c2b3e1be14fc0354b3b3ea6c0e44fc1b799fa8
JASTD <egorlinnik05@gmail.com>
2024-03-01 19:34:14 +0300

    123

--- code/main.py
+++ code/main.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder   ='templates', static_folder='static')
 
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+@app.route('/static/&lt;path:path&gt;')
+def send_static(path):
+    return send_from_directory('static', path)
 
 @app.route("/")
 def index():
-    return render_template('index.html', title = 'Про flaskович', menu=menu)
+    return render_template('index.html', title = 'Про flaskович')
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
+@app.route("/main")
+def main():
+    return render_template('main.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
